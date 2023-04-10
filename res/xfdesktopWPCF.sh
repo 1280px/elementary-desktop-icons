98dfce5f1e5609eed5402b9b7d83325742c9a5a4
1280px <71165491+1280px@users.noreply.github.com>
2023-04-10 16:12:57 +0300

    Prevent early xfdesktop init, cosmetic fixes, autostart applet

--- res/xfdesktopWPCF.sh
+++ res/xfdesktopWPCF.sh
@@ -1,16 +1,26 @@
-# start xfdesktop
-xfdesktop & disown xfdesktop 
+sleep .3     # prevent cases when script was initialized too early
+xfdesktop &
 
-# change the xfdesktop wallpaper after login
-/usr/bin/xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitoreDP-1/workspace0/last-image -s "$(gsettings get org.gnome.desktop.background picture-uri | sed -e "s/'file:\/\///" -e "s/%20/ /g" -e "s/'$//")"
+# change wallpaper function
+wpChange() {
+    wpname="$1"
 
-# change the xfdesktop wallpaper every time Gala desktop wallpaper is changed
+    # since Switchboard doesn't support using different wallpaper for desktops,
+    # we're focring xfdesktop to use one wallpaper for all workspaces,
+    # no matter if the opposite xfdesktop option is enabled.
+    /usr/bin/xfconf-query -l -c xfce4-desktop | grep "/backdrop/screen0/monitor.*/workspace.*/last-image" |
+        while read -r line; do
+            /usr/bin/xfconf-query -c xfce4-desktop -p "$line" -s "$wpname"
+        done
+}
+
+
+
+# change xfdesktop wallpaper after every login
+wpChange "$(gsettings get org.gnome.desktop.background picture-uri | sed -e "s/'file:\/\///" -e "s/%20/ /g" -e "s/'$//")"
+
+# change xfdesktop wallpaper every time Gala desktop wallpaper is changed
 /usr/bin/gsettings monitor org.gnome.desktop.background picture-uri |
     while read -r line; do
-        /usr/bin/xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitoreDP-1/workspace0/last-image -s "$(echo "$line" | sed -e "s/picture-uri: 'file:\/\///" -e "s/%20/ /g" -e "s/'$//")"
+        wpChange "$(echo "$line" | sed -e "s/picture-uri: 'file:\/\///" -e "s/%20/ /g" -e "s/'$//")"
     done
-
-notify-send -u "critical" "(dies from cringe)"
-
-
-
