47dcd27e1eb4351da9d3ca0dc87d88baf9b7cb05
1280px <71165491+1280px@users.noreply.github.com>
2023-05-16 16:24:08 +0300

    Implement "Show Desktop" routine and Win+D shortcut for it

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
-# initial setup warning
-read -e -p $'Before the installation begins, please make sure you have these \nstyle patches installed (otherwise the desktop colors will be mismatched):\n\n-- https://github.com/Romchec/elementary-OS-gtk2-support\n-- https://github.com/Romchec/eOS-non-curated-apps-theme-integration\n\nDo you want to continue the installation? [y/n] ' yn
+read -e -p $'Before the installation begins, please make sure you have these GTK style
+patches installed (otherwise the desktop colors will be mismatched):
+
+-- https://github.com/Romchec/elementary-OS-gtk2-support
+-- https://github.com/Romchec/eOS-non-curated-apps-theme-integration
+
+Do you want to continue the installation? [y/n] ' yn
 if [[ "$yn" != "Y" && "$yn" != "y" ]] ; then exit 1 ; fi
 
 
 
-# install xfdesktop and thunar (required for BG services)
-sudo apt install xfdesktop4 thunar -y
+# install all required binaries
+# (thunar is required for background services)
+sudo apt install xdotool xfdesktop4 thunar -y
 
 # hide Thunar icons from Applications menu
-echo "NoDisplay=True" >> ~/.local/share/applications/thunar.desktop
+echo "NoDisplay=True" >> ~/.local/share/applications/thunar.desktop &&
 echo "NoDisplay=True" >> ~/.local/share/applications/thunar-bulk-rename.desktop
 
 # copy xfdesktop wallpaper update script runner and add it to autorun for current user
@@ -22,7 +28,13 @@ mkdir -p ~/.config/gtk-3.0 && cp res/gtk.css ~/.config/gtk-3.0
 mkdir -p ~/.config/Thunar && cp res/uca.xml ~/.config/Thunar
 
 # copy Show Desktop script and add it to Super+D shortcut for current user
-# @TODO
+sudo cp res/showDesktop.sh /usr/local/ && sudo chmod 777 /usr/local/showDesktop.sh
+
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/customDesktop/binding "'<Super>d'" &&
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/customDesktop/command "'/usr/local/showDesktop.sh'" &&
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings \
+    "$(dconf read /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings | \
+    sed "s/]/, '\/org\/gnome\/settings-daemon\/plugins\/media-keys\/custom-keybindings\/customDesktop\/']/")"
 
 
 
