05d2e094165e93104aa5513cf3cd93b4d00b040f
1280px <71165491+1280px@users.noreply.github.com>
2023-05-19 19:06:58 +0300

    Add more checks and fix minor issues

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 read -e -p $'Before the installation begins, please make sure you have these GTK style
-patches installed (otherwise the desktop colors will be mismatched):
+patches installed (otherwise, the desktop colors might be mismatched):
 
 -- https://github.com/Romchec/elementary-OS-gtk2-support
 -- https://github.com/Romchec/eOS-non-curated-apps-theme-integration
@@ -9,15 +9,16 @@ if [[ "$yn" != "Y" && "$yn" != "y" ]] ; then exit 1 ; fi
 
 
 
-# install all required binaries
-# (thunar is required for background services)
-sudo apt install xdotool xfdesktop4 thunar -y
+# install all required dependencies
+aptToInstall="xdotool xfdesktop4 thunar"    # (thunar is required for background services)
+echo -e "\n[i] Trying to install dependencies via apt: $aptToInstall"
+sudo apt install $aptToInstall -y
 
 # hide Thunar icons from Applications menu
 echo "NoDisplay=True" >> ~/.local/share/applications/thunar.desktop &&
 echo "NoDisplay=True" >> ~/.local/share/applications/thunar-bulk-rename.desktop
 
-# copy xfdesktop wallpaper update script runner and add it to autorun for current user
+# copy xfdesktop wallpaper updater and add its launcher to autorun for current user
 sudo cp res/xfdesktopWU.sh /usr/local/ && sudo chmod 777 /usr/local/xfdesktopWU.sh
 mkdir -p ~/.config/autostart && cp res/xfdesktopWU.desktop ~/.config/autostart/
 
@@ -27,14 +28,25 @@ mkdir -p ~/.config/gtk-3.0 && cp res/gtk.css ~/.config/gtk-3.0
 # copy Open in Terminal fix for current user
 mkdir -p ~/.config/Thunar && cp res/uca.xml ~/.config/Thunar
 
-# copy Show Desktop script and add it to Super+D shortcut for current user
+# copy Show Desktop script and add it to Super+D keystroke for current user
 sudo cp res/showDesktop.sh /usr/local/ && sudo chmod 777 /usr/local/showDesktop.sh
 
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/customDesktop/binding "'<Super>d'" &&
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/customDesktop/command "'/usr/local/showDesktop.sh'" &&
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings \
-    "$(dconf read /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings | \
-    sed "s/]/, '\/org\/gnome\/settings-daemon\/plugins\/media-keys\/custom-keybindings\/customDesktop\/']/")"
+keystokesDPath="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"
+# prevent from running in case of user updating/reinstalling the script
+if !(echo "$(dconf read $keystokesDPath)" | grep -q "showDesktop"); then
+    dconf write $keystokesDPath/showDesktop/binding "'<Super>d'" &&
+    dconf write $keystokesDPath/showDesktop/command "'/usr/local/showDesktop.sh'" &&
+    dconf write $keystokesDPath "$(
+        if [[ $(echo "$(dconf read $keystokesDPath)") == "" || $(echo "$(dconf read $keystokesDPath)") == "@as []" ]]; then
+            echo "['$keystokesDPath/showDesktop/']"  # for cases when no keystrokes exist
+        else
+            echo "$(dconf read $keystokesDPath |
+            sed "s#]#, '$keystokesDPath/showDesktop/']#")"
+        fi
+    )"
+else
+    echo -e '\n[i] Skipped Show Desktop keystroke installation because it already exists'
+fi
 
 
 
