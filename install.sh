79c3e71b63be148fadf6ab497692fe2e8c90fc65
1280px <71165491+1280px@users.noreply.github.com>
2024-06-27 18:57:45 +0000

    Add Nemo Desktop!

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-read -e -p $'Before the installation begins, please make sure you have these GTK style
-patches installed (otherwise, the desktop colors might be mismatched):
+read -e -p $'
+Before the installation begins, it is recommended
+(but not necessary) to install these GTK style patches:
 
 -- https://github.com/Romchec/elementary-OS-gtk2-support
 -- https://github.com/Romchec/eOS-non-curated-apps-theme-integration
@@ -9,59 +10,97 @@ if [[ "$yn" != "Y" && "$yn" != "y" ]]; then exit 1; fi
 
 
 
-# preserve file manager defaults to set them back after Thunar installation
-currInode=$(xdg-mime query default inode/directory)
+read -e -p $'
+Please select the desktop you want to install:
 
-# install all required dependencies
-aptToInstall="xdotool xfdesktop4 thunar"    # (Thunar is required for background services)
-echo -e "\n[i] Trying to install dependencies via apt: $aptToInstall"
-sudo apt install $aptToInstall -y
+[1] nemo-desktop (DEFAULT) -- uses Nemo in background;
+    Does not require a separate script for synchronizing
+    desktop wallpaper preferences with Gala desktop.
 
-# revert file manager defaults to ones before Thunar was installed
-xdg-mime default $currInode inode/directory
+[2] xfdesktop4 -- uses XFCE4 Thunar in background;
+    Requires constantly running a separate script for syncing
+    desktop wallpaper preferences with Gala desktop.
 
-# hide Thunar icons from Applications menu
-echo "NoDisplay=True" >> ~/.local/share/applications/thunar.desktop &&
-echo "NoDisplay=True" >> ~/.local/share/applications/thunar-bulk-rename.desktop
+You can read more about each desktop pros and cons
+in the README. Your choice: ' c_desktop
 
-
-
-# copy xfdesktop wallpaper updater and add its launcher to autorun for current user
-sudo cp res/xfdesktopWU.sh /usr/local/ && sudo chmod 777 /usr/local/xfdesktopWU.sh
-mkdir -p ~/.config/autostart && cp res/xfdesktopWU.desktop ~/.config/autostart/
-
-# copy xfdesktop cosmetic patches for current user
+# apply cosmetic patches for current user
 mkdir -p ~/.config/gtk-3.0 && cp res/gtk.css ~/.config/gtk-3.0
 
-# copy xfdesktop config for current user,
-# prevent from running in case of user updating/reinstalling the script
-if [[ -f ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml ]]; then
-    echo -e '\n[i] Skipped xfdesktop config file update because it already exists'
+if [[ "$c_desktop" != "2" ]]; then
+    echo -e '\n[i] Installing minimal dependencies (risky!)...'
+    # original nemo apt package is over 200 MB, however,
+    # because we only care about the desktop, we can
+    # install just the core packages & save around 180 MB!
+    deps=(
+        "libexempi8" "libgail-3-0"
+        "libxapp1" "xapps-common" "xapp"
+        "cinnamon-desktop-data" "libcinnamon-desktop4"
+        "libnemo-extension1" "nemo-data" "nemo" 
+    )
+    for dep in "${deps[@]}"; do
+        sudo apt download "$dep" &&
+        sudo dpkg -i ./"$dep"_*.deb &
+        rm -f ./"$dep"_*.deb
+    done
+
+    # remove unwanted .desktop files
+    sudo echo "NoDisplay=True" >> /usr/share/applications/nemo.desktop
+
+    # add nemo-desktop to autorun for current user
+    mkdir -p ~/.config/autostart && cp res/nemo.desktop ~/.config/autostart/
+
+    # fix Open in Terminal action
+    gsettings set org.cinnamon.desktop.default-applications.terminal exec \
+        $(gsettings get org.gnome.desktop.default-applications.terminal exec)
+
 else
-    mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml &&
-    cp res/xfce4-desktop.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml
+    echo -e '\n[i] Installing full dependencies via apt...'
+    deps="xdotool xfdesktop4 thunar"
+    sudo apt install $deps -y
+
+    # TODO: remove unwanted .desktop files
+    sudo echo "NoDisplay=True" >> /usr/share/applications/thunar.desktop &&
+    sudo echo "NoDisplay=True" >> /usr/share/applications/thunar-bulk-rename.desktop
+
+    # copy wallpaper updater and add its launcher to autorun for current user
+    sudo cp res/xfdesktopWU.sh /usr/local/ && sudo chmod 777 /usr/local/xfdesktopWU.sh
+    mkdir -p ~/.config/autostart && cp res/xfdesktopWU.desktop ~/.config/autostart/
+
+    # copy default configs for current user,
+    # prevent from running in case of user updating/reinstalling the script
+    if [[ -f ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml ]]; then
+        echo -e '\n[i] Skipped xfdesktop config file update because it already exists'
+    else
+        mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml &&
+        cp res/xfce4-desktop.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml
+    fi
+
+    # copy Open in Terminal fix for current user
+    mkdir -p ~/.config/Thunar && cp res/uca.xml ~/.config/Thunar
 fi
 
-# copy Open in Terminal fix for current user
-mkdir -p ~/.config/Thunar && cp res/uca.xml ~/.config/Thunar
 
 
+read -e -p $'
+Now, select the Show Desktop scheme you want to use:
+
+[1] Winlike (DEFAULT) -- copies Windows/KDE "Minimize all windows".
+    Opens multitasking view if there is nothing to minimize.
+
+[2] Layered -- unique legacy scheme. Allows to peek at desktop
+    and work with newly-opened apps. Once you are done, activate it
+    again to layer new windows you need on top of the old ones.
+
+[0] None -- do not install Show Desktop scheme.
+    This will NOT uninstall the scheme if it is already installed.
+
+The scheme, if installed, will also be automatically bound
+to "Super + D" keystroke (if available). Your choice: ' c_showdesk
 
 # copy Show Desktop script scheme depending on user choice
-read -e -p $'\nPlease select Show Desktop scheme you want to use:
-
-[1] Winlike -- copies Windows/KDE "Minimize all windows" behaviour.
-    Opens multitasking view if there is nothing to minimize.
-[2] Layered -- unique legacy scheme. Allows to peek at desktop
-    and work with newly-opened apps. Once you\'re done, activate it
-    again to layer new windows you need on top of the old ones.
-[0] None -- do not install Show Desktop scheme.
-    This will NOT uninstall the scheme if it was already installed.
-
-You will be able to change your scheme later by simply running
-install.sh again (it won\'t break anything!). Your choice: ' wl
-if [[ "$wl" != "0" ]]; then
-    if [[ "$wl" != "2" ]]; then
+if [[ "$c_showdesk" != "0" ]]; then
+    if [[ "$c_showdesk" != "2" ]]; then
         showDesktopScheme="Winlike"
     else
         showDesktopScheme="Layered"
@@ -71,7 +110,7 @@ if [[ "$wl" != "0" ]]; then
     sudo mv /usr/local/showDesktop_$showDesktopScheme.sh /usr/local/showDesktop.sh &&
     sudo chmod 777 /usr/local/showDesktop.sh
 
-    # add selected Show Desktop scheme keystroke binding to Super+D,
+    # add selected Show Desktop scheme keystroke binding to Super + D,
     # prevent from running in case of user updating/reinstalling the script
     keystokesDPath="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"
     if !(echo "$(dconf read $keystokesDPath)" | grep -q "showDesktop"); then
@@ -82,7 +121,7 @@ if [[ "$wl" != "0" ]]; then
             then
                 echo "['$keystokesDPath/showDesktop/']" # for cases when no keystrokes exist
             else
-                echo "$(dconf read $keystokesDPath | sed "s|]|, '$keystokesDPath/showDesktop/']|")"
+                echo "$(dconf read $keystokesDPath | sed "s#]#, '$keystokesDPath/showDesktop/']#")"
             fi
         )"
     else
@@ -92,4 +131,5 @@ fi
 
 
 
-echo -e '\nInstallation finished! Please log out and log in again to apply the changes.'
+# done :DDDD
+echo -e '\nInstallation finished! Please log out and log back in to apply the changes.'
--- res/xfdesktopWU.sh
+++ res/xfdesktopWU.sh
@@ -2,11 +2,10 @@ sleep .3        # prevent cases when script gets initialized too early
 xfdesktop &     # (try making this value bigger if xfdesktop doesn't load on startup)
 
 
-
 # update wallpaper function
 wpUpdate() {
-    wpImage=$(echo ${1} | sed "s|^'file://||; s|%20| |g; s|'$||")
-    wpStyle=$(echo ${2} | sed "s|^'||; s|'$||")
+    wpImage=$(echo ${1} | sed "s/^'file:\/\///; s/%20/ /g; s/'$//")
+    wpStyle=$(echo ${2} | sed "s/'//g")
 
     # get current workspace if different settings
     # for multiple workspaces are ON, otherwise use primary
@@ -15,6 +14,7 @@ wpUpdate() {
     else
         currWorkspaceID=$(xdotool get_desktop)
     fi
+
 
     # change wallpaper image for given desktop of all displays
     /usr/bin/xfconf-query -l -c xfce4-desktop | grep "/backdrop/screen0/monitor.*/workspace${currWorkspaceID}/last-image" |
@@ -28,7 +28,7 @@ wpUpdate() {
         'centered') wpStyleID=1 ;;
         'spanned') wpStyleID=3 ;;
         'none') wpStyleID=0 ;;
-        *) wpStyleID=5 ;;
+        *) wpStyleID=5 ;;       # use 'zoom' as fallback value
     esac
     /usr/bin/xfconf-query -l -c xfce4-desktop | grep "/backdrop/screen0/monitor.*/workspace${currWorkspaceID}/image-style" |
         while read -r line; do
@@ -36,14 +36,16 @@ wpUpdate() {
         done
 
 
-    echo -e "UPDATED XFDESKTOP WORKSPACE #$currWorkspaceID PROPERTIES: \n  wpImage :: $wpImage\n  wpStyle :: $wpStyle\n"
+    echo -e "UPDATED XFDESKTOP WORKSPACE #$currWorkspaceID PROPERTIES: \n  wpImage :: $wpImage\n  wpStyle :: $wpStyle\n\n"
 }
 
 
-
-# update xfdesktop wallpaper when started
-wpUpdate "$(gsettings get org.gnome.desktop.background picture-uri)" \
-            "$(gsettings get org.gnome.desktop.background picture-options)"
+# update xfdesktop wallpaper when started,
+# if different settings for multiple workspaces are OFF
+if (/usr/bin/xfconf-query -c xfce4-desktop -p "/backdrop/single-workspace-mode" | grep -q "true"); then
+    wpUpdate "$(gsettings get org.gnome.desktop.background picture-uri)" \
+             "$(gsettings get org.gnome.desktop.background picture-options)"
+fi
 
 # update xfdesktop wallpaper every time Gala desktop wallpaper is changed
 gsettings monitor org.gnome.desktop.background |
