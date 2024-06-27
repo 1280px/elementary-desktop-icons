53700e4d5e0094ba633ba979f2c23b46023f1b95
1280px <71165491+1280px@users.noreply.github.com>
2024-06-28 04:40:54 +0700

    Installer fixes and improvements

--- install.sh
+++ install.sh
@@ -11,61 +11,76 @@ if [[ "$yn" != "Y" && "$yn" != "y" ]]; then exit 1; fi
 
 
 read -e -p $'
-Please select the desktop you want to install:
+First, please select the desktop you want to install:
 
 [1] nemo-desktop (DEFAULT) -- uses Nemo in background;
-    Does not require a separate script for synchronizing
-    desktop wallpaper preferences with Gala desktop.
+    Does not require a separate script for synchronizing wallpaper
+    with Gala, but needs more disk space to be installed.
 
 [2] xfdesktop4 -- uses XFCE4 Thunar in background;
-    Requires constantly running a separate script for syncing
-    desktop wallpaper preferences with Gala desktop.
+    Requires constantly running a separate script for synchronizing
+    with Gala, but more lightweight and uses lesser disk space.
 
-You can read more about each desktop pros and cons
-in the README. Your choice: ' c_desktop
+You can read more about each desktop pros & cons in README
+(well, once I actually write one). Your choice: ' c_desktop
 
 # apply cosmetic patches for current user
 mkdir -p ~/.config/gtk-3.0 && cp res/gtk.css ~/.config/gtk-3.0
 
 if [[ "$c_desktop" != "2" ]]; then
-    echo -e '\n[i] Installing minimal dependencies (risky!)...'
-    # original nemo apt package is over 200 MB, however,
-    # because we only care about the desktop, we can
-    # install just the core packages & save around 180 MB!
+    echo -e '\n[i] Installing minimal dependencies via apt...'
+    # regular nemo apt installation requires over 200 MB,
+    # however, because we only care about the desktop, we can
+    # install just the core packages and save around 180 MB!
     deps=(
         "libexempi8" "libgail-3-0"
-        "libxapp1" "xapps-common" "xapp"
+        "xapps-common" "libxapp1" "xapp"
         "cinnamon-desktop-data" "libcinnamon-desktop4"
-        "libnemo-extension1" "nemo-data" "nemo" 
+        "libnemo-extension1" "nemo-data" "nemo"
     )
     for dep in "${deps[@]}"; do
         sudo apt download "$dep" &&
-        sudo dpkg -i ./"$dep"_*.deb &
+        sudo dpkg -i ./"$dep"_*.deb &&
         rm -f ./"$dep"_*.deb
     done
 
     # remove unwanted .desktop files
+    sudo chmod 777 /usr/share/applications/nemo.desktop &&
     sudo echo "NoDisplay=True" >> /usr/share/applications/nemo.desktop
 
     # add nemo-desktop to autorun for current user
     mkdir -p ~/.config/autostart && cp res/nemo.desktop ~/.config/autostart/
 
-    # fix Open in Terminal action
+    # fix Open in Terminal action, remove Root Nemo action
     gsettings set org.cinnamon.desktop.default-applications.terminal exec \
         $(gsettings get org.gnome.desktop.default-applications.terminal exec)
+    gsettings set org.nemo.preferences.menu-config background-menu-open-as-root false
+
+    # set some default configs for current user
+    gsettings set org.nemo.desktop desktop-layout '"true::true"'
+    gsettings set org.nemo.desktop font '"Inter Bold 9.5"'
+    gsettings set org.nemo.desktop home-icon-visible true
+    gsettings set org.nemo.desktop trash-icon-visible true
+    gsettings set org.nemo.desktop volumes-visible true
+    gsettings set org.nemo.preferences click-policy '"single"'
 
 else
     echo -e '\n[i] Installing full dependencies via apt...'
-    deps="xdotool xfdesktop4 thunar"
+    deps="xfdesktop4 thunar"
     sudo apt install $deps -y
 
-    # TODO: remove unwanted .desktop files
-    sudo echo "NoDisplay=True" >> /usr/share/applications/thunar.desktop &&
+    # remove unwanted .desktop files
+    sudo chmod 777 /usr/share/applications/thunar.desktop &&
+    sudo echo "NoDisplay=True" >> /usr/share/applications/thunar.desktop
+    sudo chmod 777 /usr/share/applications/thunar-bulk-rename.desktop &&
     sudo echo "NoDisplay=True" >> /usr/share/applications/thunar-bulk-rename.desktop
 
     # copy wallpaper updater and add its launcher to autorun for current user
-    sudo cp res/xfdesktopWU.sh /usr/local/ && sudo chmod 777 /usr/local/xfdesktopWU.sh
+    sudo cp res/xfdesktopWU.sh /usr/local/ && sudo chmod 755 /usr/local/xfdesktopWU.sh
     mkdir -p ~/.config/autostart && cp res/xfdesktopWU.desktop ~/.config/autostart/
+
+    # copy Open in Terminal fix for current user
+    mkdir -p ~/.config/Thunar && cp res/uca.xml ~/.config/Thunar
 
     # copy default configs for current user,
     # prevent from running in case of user updating/reinstalling the script
@@ -75,14 +90,15 @@ else
         mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml &&
         cp res/xfce4-desktop.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml
     fi
-
-    # copy Open in Terminal fix for current user
-    mkdir -p ~/.config/Thunar && cp res/uca.xml ~/.config/Thunar
 fi
 
 
 
-read -e -p $'
+# prevent from running in case of user updating/reinstalling the script
+keystokesDPath="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"
+
+if !(echo "$(dconf read $keystokesDPath)" | grep -q "showDesktop"); then
+    read -e -p $'
 Now, select the Show Desktop scheme you want to use:
 
 [1] Winlike (DEFAULT) -- copies Windows/KDE "Minimize all windows".
@@ -98,22 +114,21 @@ Now, select the Show Desktop scheme you want to use:
 The scheme, if installed, will also be automatically bound
 to "Super + D" keystroke (if available). Your choice: ' c_showdesk
 
-# copy Show Desktop script scheme depending on user choice
-if [[ "$c_showdesk" != "0" ]]; then
-    if [[ "$c_showdesk" != "2" ]]; then
-        showDesktopScheme="Winlike"
-    else
-        showDesktopScheme="Layered"
-    fi
+    # copy Show Desktop script scheme depending on user choice
+    if [[ "$c_showdesk" != "0" ]]; then
+        echo -e '\n[i] Installing xdotool dependency via apt...'
+        sudo apt install xdotool -y
 
-    sudo cp res/showDesktop_$showDesktopScheme.sh /usr/local/ &&
-    sudo mv /usr/local/showDesktop_$showDesktopScheme.sh /usr/local/showDesktop.sh &&
-    sudo chmod 777 /usr/local/showDesktop.sh
+        if [[ "$c_showdesk" != "2" ]]; then
+            showDesktopScheme="Winlike"
+        else
+            showDesktopScheme="Layered"
+        fi
+        sudo cp res/showDesktop_$showDesktopScheme.sh /usr/local/ &&
+        sudo mv /usr/local/showDesktop_$showDesktopScheme.sh /usr/local/showDesktop.sh &&
+        sudo chmod 755 /usr/local/showDesktop.sh
 
-    # add selected Show Desktop scheme keystroke binding to Super + D,
-    # prevent from running in case of user updating/reinstalling the script
-    keystokesDPath="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"
-    if !(echo "$(dconf read $keystokesDPath)" | grep -q "showDesktop"); then
+        # add selected Show Desktop scheme keystroke binding to Super + D
         dconf write $keystokesDPath/showDesktop/binding "'<Super>d'" &&
         dconf write $keystokesDPath/showDesktop/command "'/usr/local/showDesktop.sh'" &&
         dconf write $keystokesDPath "$(
@@ -124,12 +139,12 @@ if [[ "$c_showdesk" != "0" ]]; then
                 echo "$(dconf read $keystokesDPath | sed "s#]#, '$keystokesDPath/showDesktop/']#")"
             fi
         )"
-    else
-        echo -e '\n[i] Skipped Show Desktop keystroke installation because it already exists'
     fi
+else
+    echo -e '\n[i] Skipped Show Desktop scheme installation because it already exists'
 fi
 
 
 
 # done :DDDD
-echo -e '\nInstallation finished! Please log out and log back in to apply the changes.'
+echo -e '\nInstallation finished! Please log out and log back in to apply the changes.\n'
